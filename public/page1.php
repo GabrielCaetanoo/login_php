e0bb5bf4d6bd58c7b704bcf225c05e20168aaeb8
Gabriel Caetano <gabrielc0202@hotmail.com>
2024-10-15 20:30:59 -0300

    pages

--- public/page1.php
+++ public/page1.php
@@ -7,15 +7,15 @@ defined('CONTROL') or die('Acesso Negado!');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App</title>
+    <title>Pagina 1</title>
 </head>
 <body>
 
     <?php require 'nav.php' ?>    
 
-    <h3>Bem-vindo a aplicação</h3>
+    <h3>Pagina 1</h3>
+    <hr>
     [Conteudo]
-
     
 </body>
 </html>
